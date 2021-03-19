53c4daaaf5824e4a45fe6560038d77ce49a3d07c
Madushan Sarathbabu <Madushansarathbabu@outlook.com>
2021-03-19 17:53:04 +0530

    Update music.php

--- music.php
+++ music.php
@@ -6,23 +6,86 @@
 	<title>MusicBoax</title>
   <link rel="shortcut icon" href="Images/Icon.jpg" type="image/jpg">
   <link rel="stylesheet" type="text/css" href="CSS/design.css">
+       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+       <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+       <link rel="stylesheet"  href="https://code.jquery.com/jquery-3.3.1.slim.min.js">
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js">
 </head>
-<body>
+<body class="column">
+
 <div id="main">
     <nav role = "main">
         <img id="logo" src="Images/logo.jpg" alt="temporarylogo" >
        <nav class="nav-bar">
-    <ul>
-  <li><a href="Music.php">Home</a></li>
-  <li><a href="Report.php">Report Bug</a></li>
-  <li><a href="Form.php">Contact</a></li>
-  <li><a href="About.php">About</a></li>
-   <li><a href="Donation.php">Buy Kofi</a></li>
-</ul>
+
+<nav class="navbar">
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
+</nav>
   </nav>
-</div>
+</nav>
 </div>
 
+
+  
+
+ <!-- Footer -->
+  <footer class="bg-white">
+    <div class="container py-5">
+      <div class="row py-4">
+        <div class="col-lg-4 col-md-6 mb-4 mb-lg-0"><img src="img/logo.png" alt="" width="180" class="mb-3">
+          <p class="font-italic text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+          <ul class="list-inline mt-4">
+            <li class="list-inline-item"><a href="#"  title="twitter"><i class="fa fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href="#" target="_blank" title="facebook"><i class="fa fa-facebook"></i></a></li>
+            <li class="list-inline-item"><a href="#" target="_blank" title="instagram"><i class="fa fa-instagram"></i></a></li>
+            <li class="list-inline-item"><a href="#" target="_blank" title="reddit"><i class="fa fa-reddit" aria-hidden="true"></i></a></li>
+            <li class="list-inline-item"><a href="#" target="_blank" title="Youtube"><i class="fa fa-youtube"></i></a></li>
+          </ul>
+        </div>
+       <!-- <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
+          <h6 class="text-uppercase font-weight-bold mb-4">Shop</h6>
+          <ul class="list-unstyled mb-0">
+            <li class="mb-2"><a href="#" class="text-muted">For Women</a></li>
+            <li class="mb-2"><a href="#" class="text-muted">For Men</a></li>
+            <li class="mb-2"><a href="#" class="text-muted">Stores</a></li>
+            <li class="mb-2"><a href="#" class="text-muted">Our Blog</a></li>
+          </ul>
+        </div>-->
+        <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
+          <h6 class="text-uppercase font-weight-bold mb-4">Company</h6>
+          <ul class="list-unstyled mb-0">
+            <li class="mb-2"><a href="#" class="text-muted">Login</a></li>
+            <li class="mb-2"><a href="#" class="text-muted">Register</a></li>
+            <li class="mb-2"><a href="#" class="text-muted">Report Bug</a></li>
+            <li class="mb-2"><a href="#" class="text-muted">Donate</a></li>
+          </ul>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-lg-0">
+          <h6 class="text-uppercase font-weight-bold mb-4">Subscribe to get notified of new songs</h6>
+          <p class="text-muted mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At itaque temporibus.</p>
+          <div class="p-1 rounded border">
+            <div class="input-group">
+              <input type="email" placeholder="Enter your email address" aria-describedby="button-addon1" class="form-control border-0 shadow-0">
+              <div class="input-group-append">
+                <button id="button-addon1" type="submit" class="btn btn-link"><i class="fa fa-paper-plane"></i></button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Copyrights -->
+    <div class="bg-light py-4">
+      <div class="container text-center">
+        <p class="text-muted mb-0 py-2">© 2021 MusicBoax All rights reserved.</p>
+      </div>
+    </div>
+  </footer>
+  <!-- End -->
 
 </body>
 </html>
